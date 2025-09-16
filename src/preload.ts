6f4be85823ffb88333329afd7529f4a43206889c
moondogdev <moondogdev@gmail.com>
2025-09-16 10:44:33 -0400

    feat: Implement save state management and UI

--- src/preload.ts
+++ src/preload.ts
@@ -9,11 +9,16 @@ contextBridge.exposeInMainWorld('electronAPI', {
   importProject: () => ipcRenderer.invoke('dialog:importProject'),
   openExternalLink: (url: string) => ipcRenderer.send('open-external-link', url),
   showContextMenu: () => ipcRenderer.send('show-context-menu'),
+  getStoreValue: (key: string) => ipcRenderer.invoke('electron-store-get', key),
+  setStoreValue: (key: string, value: any) => ipcRenderer.send('electron-store-set', key, value),
+  send: (channel: string) => ipcRenderer.send(channel), // Expose a generic send method
+  notifyDirtyState: (isDirty: boolean) => ipcRenderer.send('notify-dirty-state', isDirty),
   // It's good practice to define which channels are allowed for two-way communication
-  on: (channel: string, callback: (event: IpcRendererEvent, ...args: any[]) => void) => {
-    const validChannels = ['your-valid-channel-here']; // Example, not used yet but good practice
+  on: (channel: string, callback: (...args: any[]) => void) => {
+    const validChannels = ['prepare-for-shutdown', 'save-and-shutdown'];
     if (validChannels.includes(channel)) {
       ipcRenderer.on(channel, callback);
+      return () => ipcRenderer.removeListener(channel, callback);
     }
   },
 });
